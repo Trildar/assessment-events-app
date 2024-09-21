e4f141c618578b38a8127f2d8a2dfad9c7d40809
Jaydon Soo <jaydonsky@gmail.com>
2024-09-22 03:34:12 +0800

    Add static file serving for event thumbnails

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use("/store", express.static("store"));
 app.use("/admin", routes_admin);
 app.use("/event", routes_event);
 
